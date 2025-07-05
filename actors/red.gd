fb1ab2ce2e0823fc490f4f3da5638c899e8e80b3
ducky <duckyart@gmail.com>
2025-07-05 20:54:09 +0300

    Cleaned up red.gd

--- actors/red.gd
+++ actors/red.gd
@@ -4,15 +4,11 @@ extends CharacterBody2D
 var jump_curve_time : float = 10.0
 @export var walk_curve : Curve
 
-var gravity_effect : float = 0.0
-var walk_effect : float = 0.0
+
+
 
 var grounded_timer:float = 0.0
-var walk_timer: float = 0.0
 var jump_timer: float = 0.0
-var attack_timer: float = 10.0
-
-var invisible_time : float = 0.0
 
 @export var projectiles: Array[PackedScene]
 
@@ -30,92 +26,95 @@ const ATTACK = "attack"
 const COOLDOWN = "cooldown"
 const DAMAGE = "damage"
 
-var control_state := [UNKNOWN, UNKNOWN]
-var horizontal_state := [UNKNOWN, UNKNOWN]
-var vertical_state := [UNKNOWN, UNKNOWN]
-var attack_state := [UNKNOWN, UNKNOWN, UNKNOWN] # third is equipment
+# States: Current, Previous, Current age, Previous age
+var control_state := [UNKNOWN, UNKNOWN, 0.0, 0.0]
+var horizontal_state := [UNKNOWN, UNKNOWN, 0.0, 0.0]
+var vertical_state := [UNKNOWN, UNKNOWN, 0.0, 0.0]
+var attack_state := [UNKNOWN, UNKNOWN, 0.0, 0.0]
+
+var states = [control_state, horizontal_state, vertical_state, attack_state]
 
 
 func fuzzy(a, b = 0.1):
 	return a > 0 and a < b
 
 func hit(source : Node, type = DAMAGE) -> void:
-	if invisible_time > Game.invulnerability_time:
-		control_state[0] = type
+	pass
+
+func change_state(state_group:Array, state) -> void:
+	state_group[0] = state
+	state_group[3] = state_group[2]
+	state_group[2] = 0.0
 
 func _physics_process(delta: float) -> void:
-	var input_left = Input.is_action_pressed("move_left")
-	var input_right = Input.is_action_pressed("move_right")
-	var input_vector = Input.get_vector("move_left", "move_right", "Down", "Up")
-	var input_sign = Vector2i(sign(input_vector.x), sign(input_vector.y))
-	var attack = Input.is_action_just_pressed("Fire")
+	var walk_effect : float = 0.0
+	var gravity_effect : float = 0.0
+	
+	var input_left := Input.is_action_pressed("move_left")
+	var input_right := Input.is_action_pressed("move_right")
+	var input_vector := Input.get_vector("move_left", "move_right", "Down", "Up")
+	var input_sign := Vector2i(sign(input_vector.x), sign(input_vector.y))
+	var attack := Input.is_action_just_pressed("Fire")
 	
 	grounded_timer = max(delta, grounded_timer + delta) if is_on_floor() else min(-delta, grounded_timer - delta)
 	jump_timer = max(delta, jump_timer + delta) if Input.is_action_pressed("jump") else min(-delta, jump_timer - delta)
-	attack_timer += delta
-	invisible_time += delta
 
-	control_state[1] = control_state[0]
-	vertical_state[1] = vertical_state[0]
-	horizontal_state[1] = horizontal_state[0]
-	attack_state[1] = attack_state[0]
+	for state in states:
+		state[1] = state[0]
+		state[2] += delta
 	
 	# Adjust only state
-	match control_state:
-		[UNKNOWN, ..]:
-			control_state[0] = READY
-	
 	match attack_state:
-		[UNKNOWN,_,_]:
-			attack_state[0] = READY
-		[ATTACK,_,_]: # Previously attacked, return to Ready
-			attack_state[0] = READY
-		[READY,_,_] when attack and attack_timer > 0.1:
-			attack_state[0] = ATTACK
+		[UNKNOWN, ..]:
+			change_state(attack_state, READY)
+		[ATTACK, ..]: # Previously attacked, return to Ready
+			change_state(attack_state, READY)
+		[READY, _, var age, ..] when attack and age > 0.1:
+			change_state(attack_state, ATTACK)
+	
+	
 	
 	match vertical_state:
-		[UNKNOWN, _]: # Unknown
-			vertical_state[0] = GROUNDED if grounded_timer > 0 else FALLING
-		[JUMPING, _] when jump_timer < 0 or jump_timer > jump_curve.get_point_position(1).x: # stop jump
-			vertical_state[0] = FALLING
-		[FALLING, _] when grounded_timer > 0: # Landed
-			vertical_state[0] = GROUNDED
+		[UNKNOWN, ..]: # Unknown
+			change_state(vertical_state, GROUNDED if grounded_timer > 0 else FALLING)
+		[JUMPING, ..] when jump_timer < 0 or jump_timer > jump_curve.get_point_position(1).x: # stop jump
+			change_state(vertical_state, FALLING)
+		[FALLING, ..] when grounded_timer > 0: # Landed
+			change_state(vertical_state, GROUNDED)
 			if fuzzy(jump_timer): # Jump on land
 				vertical_state[1] = GROUNDED
 				vertical_state[0] = JUMPING
-		[FALLING, _] when fuzzy(-grounded_timer) and fuzzy(jump_timer): # coyote
-			vertical_state[0] = JUMPING
-		[GROUNDED, _] when fuzzy(jump_timer): # jump on ground
-			vertical_state[0] = JUMPING
-		[GROUNDED, _] when grounded_timer < 0:
-			vertical_state[0] = FALLING
+		[FALLING, ..] when fuzzy(-grounded_timer) and fuzzy(jump_timer): # coyote
+			change_state(vertical_state, JUMPING)
+		[GROUNDED, ..] when fuzzy(jump_timer): # jump on ground
+			change_state(vertical_state, JUMPING)
+		[GROUNDED, ..] when grounded_timer < 0:
+			change_state(vertical_state, FALLING)
 
 	match horizontal_state:
-		[UNKNOWN, _]: #Unknown
-			horizontal_state[0] = IDLE
-		[IDLE, _] when input_left != input_right: # start walking
-			horizontal_state[0] = WALKING
-		[WALKING, _] when not input_left and not input_right: # stop walking
-			horizontal_state[0] = IDLE
+		[UNKNOWN, ..]: #Unknown
+			change_state(horizontal_state, IDLE)
+		[IDLE, ..] when input_left != input_right: # start walking
+			change_state(horizontal_state, WALKING)
+		[WALKING, ..] when not input_left and not input_right: # stop walking
+			change_state(horizontal_state, IDLE)
 	
 	# React to state
 	
-	
 	match vertical_state:
-		[GROUNDED, _]:
+		[GROUNDED, ..]:
 			jump_curve_time = jump_curve.get_point_position(1).x
-		[JUMPING, GROUNDED]:
+		[JUMPING, GROUNDED, ..]:
 			jump_curve_time = 0.0
-		[FALLING, JUMPING]:
+		[FALLING, JUMPING, ..]:
 			jump_curve_time = jump_curve.get_point_position(1).x
-		[FALLING, FALLING]:
+		[FALLING, FALLING, ..]:
 			if jump_curve_time >= jump_curve.get_point_position(2).x:
 				jump_curve_time = jump_curve.get_point_position(2).x - delta
 	
 	match horizontal_state:
-		[WALKING, _]:
-			walk_timer = max(delta, walk_timer + delta)
-			var walk_speed = walk_curve.sample_baked(walk_timer)
+		[WALKING, _, var age, ..]:
+			var walk_speed = walk_curve.sample_baked(age)
 			walk_effect = walk_speed if input_right else -walk_speed
 			
 			if $VisualRoot.scale.x != -input_sign.x:
@@ -123,12 +122,11 @@ func _physics_process(delta: float) -> void:
 				if s.x != 0:
 					$VisualRoot.scale = s
 					input_sign.x
-		[IDLE, _]:
-			walk_timer = min(-delta, walk_timer - delta)
+		[IDLE, ..]:
 			walk_effect = 0.0
 	
 	match attack_state:
-		[ATTACK,..]:
+		[ATTACK, _, _, var prev_age]:
 			var bullet = projectiles[0]
 			var placement: Node2D = $VisualRoot/AimLoc/AimForward
 			match [abs(input_sign.x), input_sign.y]:
@@ -143,8 +141,7 @@ func _physics_process(delta: float) -> void:
 						placement = $VisualRoot/AimLoc/AimCrouched
 					else:
 						placement = $VisualRoot/AimLoc/AimDown
-			shoot.emit(bullet, placement.global_transform, attack_timer)
-			attack_timer = 0.0
+			shoot.emit(bullet, placement.global_transform, prev_age)
 	
 	var a := jump_curve.sample(jump_curve_time)
 	jump_curve_time += delta
