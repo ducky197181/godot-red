4571ee3de61ecd483d4a8bc13a2cd478b0f544cc
ducky <duckyart@gmail.com>
2025-07-06 16:47:28 +0300

    Clean up project root

--- core/game.gd
+++ core/game.gd
@@ -1,16 +1,16 @@
 extends Node
 
-
+enum GameState { PLAY_STATE, LOAD_MAP }
 
 func _init() -> void:
 	var config := ConfigFile.new()
-	var err = config.load("res://config.cfg")
-	if err != OK:
-		return
+	var err = config.load("res://core/config.cfg")
+	assert(err == OK)
 	invulnerability_time = config.get_value("gameplay", "invulnerability_time", 1.0)
 
 var player_health := 20
 var invulnerability_time : float
+var game_state := GameState.PLAY_STATE
 
 signal player_health_change(value: int)
 
@@ -21,3 +21,12 @@ func damage_player(value: int):
 		return
 	player_health = target
 	player_health_change.emit(player_health)
+
+func load_map(path: String):
+	if game_state == GameState.LOAD_MAP:
+		push_warning("Loading map while one is loading? Abort.")
+		return false
+		
+	var rt = get_tree()
+	
+	pass
