b9d4b52f06f7c73e809965798b314b07613f5746
ducky <duckyart@gmail.com>
2025-07-11 15:28:04 +0300

    Damage input standardization
    Closes #11
    `hit` function has been replaced with `command(cmd:Dictionary)`, which is more open ended about what effect the script is trying to add to the target. Initially we use only keys `damage` and `source`.

--- core/area_damage.gd
+++ core/area_damage.gd
@@ -1,8 +1,15 @@
 extends Area2D
 
 @export var damage:int = 1
+var hit_dictionary : Dictionary
 
-func _physics_process(delta: float) -> void:
+func _ready() -> void:
+	hit_dictionary = {
+		"damage": damage
+	}
+	hit_dictionary.make_read_only()
+
+func _physics_process(_delta) -> void:
 	for body in get_overlapping_bodies():
-		if body.has_method("hit"):
-			body.hit(null, "damage")
+		if body.has_method("command"):
+			body.command(hit_dictionary)
