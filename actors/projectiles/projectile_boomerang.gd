4b89f9b1b318ea028263542572ff8a5de159e821
ducky <duckyart@gmail.com>
2025-07-17 20:11:31 +0300

    Convert player projectile to CharacterBody2D, allows closing #19

--- actors/projectiles/projectile_boomerang.gd
+++ actors/projectiles/projectile_boomerang.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-@onready var projectile : Area2D = $ProjectileA
+@onready var projectile : CharacterBody2D = $ProjectileA
 @onready var target : Node2D = $Marker2D
 
 var moving_target : Node2D
@@ -23,6 +23,8 @@ func set_moving_target(tg : Node2D, launch := 0, timer := 1.0):
 	target.position.x *= timer
 	moving_target = tg
 	speed += clampf(launch, 20.0, 70.0)
+	dir = (target.global_position - projectile.global_position).normalized().angle()
+	projectile.add_collision_exception_with(tg)
 
 func _physics_process(delta: float) -> void:
 	if projectile.position.distance_squared_to(target.position) < 8:
@@ -32,23 +34,31 @@ func _physics_process(delta: float) -> void:
 		target.global_position = target.global_position + global_diff
 		turnspeed *= 1.2
 		speed *= 1.2
+		for exception in projectile.get_collision_exceptions():
+			projectile.remove_collision_exception_with(exception)
 	
 	age += delta
-	var target_dir = (target.position - projectile.position).normalized().angle()
+	var target_dir = (target.global_position - projectile.global_position).normalized().angle()
 	dir = rotate_toward(dir, target_dir, delta*turnspeed)
 
 	var v = Vector2.RIGHT.rotated(dir)
-	projectile.position = projectile.position + (v * delta * speed)
+	var mc = projectile.move_and_collide(v*delta*speed)
+	if mc:
+		_on_projectile_a_body_entered(mc.get_collider())
+		
 	$ProjectileA/Plh.rotate(PI * delta)
 	Game.ghost_trail($ProjectileA/Plh)
 
 
 func _on_projectile_a_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player_projectile_target") and body.has_method("command"):
+	if body.is_in_group("player") and age < 1.0:
+		
+		return
+		
+	if not body.is_in_group("player") and body.has_method("command"):
 		body.command(command_obj)
-		queue_free()
-	if age > 1.0 and body.is_in_group("player"):
-		queue_free()
+		
+	queue_free()
 
 
 func _on_projectile_a_area_entered(area: Area2D) -> void:
